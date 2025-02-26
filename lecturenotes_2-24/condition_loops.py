68a3dcc366d9d0c50c86d239a9c0dec5a44e5c23
Jordan Reed <jlreed@students.nic.edu>
2025-02-26 19:51:09 +0000

    changed indentation to 4 spaces

--- lecturenotes_2-24/condition_loops.py
+++ lecturenotes_2-24/condition_loops.py
@@ -8,49 +8,49 @@ age2 = 15
 
 # if else example
 if age > 21:
-  print("you are an adult")
-  print("You are old")
+    print("you are an adult")
+    print("You are old")
 else:
-  print("You are young.")
+    print("You are young.")
 
 # if elif and program flow
 age = 30  # difference = 15
 difference = age - age2
 if difference >= 0:
-  print("positive or zero")
+    print("positive or zero")
 elif difference > 5:
-  print("big number")
+    print("big number")
 elif difference < 0:
-  print("negative")
+    print("negative")
 
 if difference > 5:
-  print("big number")
+    print("big number")
 
 # while loop with counter
 counter = 0
 while counter <= 10:
-  print(counter)
-  # increment counter
-  # counter = counter + 1
-  counter += 1
+    print(counter)
+    # increment counter
+    # counter = counter + 1
+    counter += 1
 
 # for loop
 inventory = ['sword', 'shield', 'key']
 for item in inventory:
-  print(item)
+    print(item)
 
 # boolean operators
 is_game_paused = False
 is_dead = True
 
 if is_game_paused or is_dead:
-  print('game paused')
+    print('game paused')
 else:
-  print('game not paused')
+    print('game not paused')
 
 if is_game_paused and is_dead:
-  print('game over')
+    print('game over')
 
 health = 50
 if health == 0:
-  is_dead = True
+    is_dead = True
