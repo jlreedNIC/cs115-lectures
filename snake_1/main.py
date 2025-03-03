5ee380541f025827bf75a05d970071ed6402aca7
Jordan Reed <jlreed@students.nic.edu>
2025-03-03 20:14:07 +0000

    updated snake game to include the moving snake

--- snake_1/main.py
+++ snake_1/main.py
@@ -1,8 +1,8 @@
 # -----
-# Start to the snake game using PyGame
-# Only creates a PyGame screen
+# Snake game using PyGame
 # -----
 import pygame
+from pygame.constants import KEYDOWN
 
 # init pygame
 pygame.init()
@@ -19,6 +19,13 @@ pygame.display.set_caption("snake")
 clock = pygame.time.Clock()
 dt = 0
 speed = 10
+
+# snake current position
+cur_pos = [300, 200]
+direction = "down"  # direction of snake traveling
+snake_body = [  # snake body pieces
+    [300, 200], [300, 180], [300, 160], [300, 140]
+]
 """game loop"""
 running = True
 while running:
@@ -26,41 +33,59 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == KEYDOWN:
+            if event.key == pygame.K_ESCAPE:  # escape key
+                running = False
+
+            if event.key == pygame.K_w and direction != "down":  # up direction
+                direction = "up"
+            if event.key == pygame.K_s and direction != "up":  # down direction
+                direction = "down"
+            if event.key == pygame.K_a and direction != "right":  # left direction
+                direction = "left"
+            if event.key == pygame.K_d and direction != "left":  # right direction
+                direction = "right"
     """update our game state"""
+    # update direction
+    if direction == "up":
+        cur_pos[1] -= 20
+    if direction == "down":
+        cur_pos[1] += 20
+    if direction == "left":
+        cur_pos[0] -= 20
+    if direction == "right":
+        cur_pos[0] += 20
+
+    # update bounds
+    if cur_pos[0] < 0:  # x direction bounds
+        cur_pos[0] = 0
+        running = False
+    if cur_pos[0] > width - 20:
+        cur_pos[0] = width - 20
+        running = False
+
+    if cur_pos[1] < 0:  # y direction bounds
+        cur_pos[1] = 0
+        running = False
+    if cur_pos[1] > height - 20:
+        cur_pos[1] = height - 20
+        running = False
+
+    # snake movement
+    snake_body.insert(0, list(cur_pos))
+    snake_body.pop()
+
+    # handle lose state
+    # insert losing code here
     """ draw to our screen """
     # clear screen
     screen.fill("white")
 
-    # draw rectangle
-    pygame.draw.rect(screen, "green", pygame.Rect((100, 200), (100, 50)))
-
-    # rectangle with parameters specified
-    pygame.draw.rect(
-        surface=screen,
-        color=(200, 128, 50),                # brown color
-        rect=pygame.Rect(540, 340, 50, 50)   # create a rectangle at (540,340)
-                                             # that has height and width of 50
-    )
-
-    # draw circle
-    pygame.draw.circle(screen, "blue", (100, 200), 40)
-
-    # circle with parameters specified
-    pygame.draw.circle(
-        surface=screen, 
-        color="black", 
-        center=(200, 200),        # put center of circle at point (200, 200)
-        radius=100                # give circle a radius of 100
-    )
-
-    # draw line
-    pygame.draw.line(screen, "red", (450, 100), (200, 200), 5)
-
-    # ellipse with parameters specified
-    pygame.draw.ellipse(surface=screen,
-                        color="red",
-                        rect=pygame.Rect((100, 100),(100, 500)),
-                       )
+    # draw snake
+    for body in snake_body:
+        pygame.draw.rect(surface=screen,
+                         color="green",
+                         rect=pygame.Rect(body[0], body[1], 20, 20))
 
     # update screen
     pygame.display.flip()
