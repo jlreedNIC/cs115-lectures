46887261472d09cee6b39e1c762bc04fbf76a98a
Jordan Reed <jlreed@students.nic.edu>
2025-03-05 19:40:37 +0000

    function to demonstrate local and global variables

---
+++ lecturenotes_3-5/functions.py
@@ -0,0 +1,16 @@
+print('Good Morning!')
+# doubled_num = 0
+
+def double(num):
+    # global doubled_num
+    doubled_num = num*2
+    print(doubled_num)
+
+    return doubled_num
+
+a = 5
+new_variable = double(a)
+print(new_variable)
+
+
+    
